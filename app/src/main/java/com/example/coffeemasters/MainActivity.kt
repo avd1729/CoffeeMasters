a9cf615358b80c1903d1b6781756cdbcd96b46ad
Aravind <aravind19112003@gmail.com>
2024-12-20 23:11:54 +0530

    Working with composables

--- app/src/main/java/com/example/coffeemasters/MainActivity.kt
+++ app/src/main/java/com/example/coffeemasters/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.coffeemasters.ui.theme.CoffeeMastersTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoffeeMastersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FirstComposable()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CoffeeMastersTheme {
-        Greeting("Android")
+fun FirstComposable() {
+//    Column(){
+//        Text(text = "Hello Again",
+//            modifier = Modifier
+//                .background(Color.Cyan)
+//                .padding(20.dp)
+//        )
+//
+//        Text(text = "Heyy",
+//            modifier = Modifier
+//                .background(Color.Red)
+//                .padding(20.dp))
+//    }
+
+    Row(){
+        Text(text = "Heyy",
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(20.dp))
+
+        Text(text = "Hello Again",
+            modifier = Modifier
+                .background(Color.Cyan)
+                .padding(20.dp)
+        )
     }
 }
