ae9a1e99c9f142fdf4a6be5ef14531d6b72504ae
Aravind <aravind19112003@gmail.com>
2024-12-21 00:27:27 +0530

    Working with svgs

--- app/src/main/java/com/example/coffeemasters/OffersPage.kt
+++ app/src/main/java/com/example/coffeemasters/OffersPage.kt
@@ -1,8 +1,9 @@
 package com.example.coffeemasters
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -10,11 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.coffeemasters.ui.theme.Purple80
 
 @Preview(showBackground = true, widthDp = 400)
 @Composable
@@ -26,18 +28,21 @@ fun Offer() {
             fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
             fontFamily = androidx.compose.ui.text.font.FontFamily.SansSerif
         )
-
+    Image(painter = painterResource(R.drawable.background_pattern),
+        contentDescription = "Background Pattern",
+        contentScale = ContentScale.FillWidth,
+        modifier = Modifier
+            .fillMaxWidth()
+    )
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .background(Purple80)
             .padding(16.dp)
     ) {
         Text(text = "My Offer",
             fontSize = 16.sp,
             style = style
         )
-
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Up to 30% off",
             style = MaterialTheme.typography.displayMedium,
