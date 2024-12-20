9f5cdfe3711b02780c5310229bb86045bf88f808
Aravind <aravind19112003@gmail.com>
2024-12-21 00:13:16 +0530

    Working with colors

--- app/src/main/java/com/example/coffeemasters/OffersPage.kt
+++ app/src/main/java/com/example/coffeemasters/OffersPage.kt
@@ -1,13 +1,22 @@
 package com.example.coffeemasters
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.coffeemasters.ui.theme.Purple80
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 400)
 @Composable
 fun Offer() {
 
@@ -18,12 +27,22 @@ fun Offer() {
             fontFamily = androidx.compose.ui.text.font.FontFamily.SansSerif
         )
 
-    Column() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(Purple80)
+            .padding(16.dp)
+    ) {
         Text(text = "My Offer",
             fontSize = 16.sp,
             style = style
         )
-        Text(text = "Up to 30% off")
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Up to 30% off",
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier.padding(10.dp)
+        )
     }
 
 }
--- app/src/main/java/com/example/coffeemasters/ui/theme/Theme.kt
+++ app/src/main/java/com/example/coffeemasters/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.coffeemasters.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -18,9 +17,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Primary,
+    secondary = Secondary,
+    tertiary = Ternary
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
