06f2dbcd4b9909b1f20254173053c6c504b2ae25
Aravind <aravind19112003@gmail.com>
2024-12-21 00:38:12 +0530

    Working with offer page

--- app/src/main/java/com/example/coffeemasters/OffersPage.kt
+++ app/src/main/java/com/example/coffeemasters/OffersPage.kt
@@ -1,11 +1,14 @@
 package com.example.coffeemasters
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,9 +21,25 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview(showBackground = true, widthDp = 400)
+@Preview(showBackground = true)
 @Composable
-fun Offer() {
+private fun Offer_Preview() {
+    Offer("Christmas Offer", "Up to 20% off during Holidays")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OffersPage() {
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+    ) {
+        Offer(title = "Early Coffe", description = "10% off. Offer valid from 6am to 9am.")
+        Offer(title = "Welcome Gift", description = "25% off on your first order.")
+    }
+}
+
+@Composable
+fun Offer(title: String, description: String) {
 
     val style =
         TextStyle(
@@ -28,6 +47,10 @@ fun Offer() {
             fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
             fontFamily = androidx.compose.ui.text.font.FontFamily.SansSerif
         )
+
+    Box(
+        modifier = Modifier.padding(16.dp)
+    )
     Image(painter = painterResource(R.drawable.background_pattern),
         contentDescription = "Background Pattern",
         contentScale = ContentScale.FillWidth,
@@ -38,15 +61,18 @@ fun Offer() {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(16.dp)
+            .fillMaxWidth()
     ) {
-        Text(text = "My Offer",
+        Text(text = title,
             fontSize = 16.sp,
             style = style
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Up to 30% off",
+        Text(text = description,
             style = MaterialTheme.typography.displayMedium,
-            modifier = Modifier.padding(10.dp)
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
         )
     }
 
